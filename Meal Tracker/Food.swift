2676643d4cb765701dd2f24cfab12facf5389d73
derryev <derryev@ssfs.org>
2024-04-22 15:10:00 -0400

    v1

---
+++ Meal Tracker/Food.swift
@@ -0,0 +1,16 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Eva Derryberry on 4/22/24.
+//
+
+import Foundation
+
+
+
+struct Food {
+    var name: String
+    var description: String
+    
+}
---
+++ Meal Tracker/Meal.swift
@@ -0,0 +1,15 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by Eva Derryberry on 4/22/24.
+//
+
+import Foundation
+
+
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
